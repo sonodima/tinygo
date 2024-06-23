bb42710626ea25fbc64786462aeec683bb04fa7b
Ayke van Laethem <aykevanlaethem@gmail.com>
2024-06-23 14:42:25 +0200

    unique: implement custom version of unique package
    This version probably isn't as fast as the upstream version, but it is
    good enough for now. It also doesn't free unreferenced handles like the
    upstream version.

--- loader/goroot.go
+++ loader/goroot.go
@@ -249,6 +249,7 @@ func pathsToOverride(goMinor int, needsSyscallPackage bool) map[string]bool {
 		"runtime/":              false,
 		"sync/":                 true,
 		"testing/":              true,
+		"unique/":               false,
 	}
 
 	if goMinor >= 19 {
